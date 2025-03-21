df67f499e5be3c3ae268ab23814cad99f3ec2920
Quentin Kaiser <quentin.kaiser@onekey.com>
2025-03-21 08:56:55 +0100

    feat: add support for 64-bit symbol lookup tables
    To overcome the 4 GiB file size limit some operating system like Solaris
    11.2 and GNU use a variant lookup table. Instead of 32-bit integers,
    64-bit integers are used in the symbol lookup tables. The string
    "/SYM64/" instead "/" is used as identifier for this table[0].
    
    A 32-bit archive symbol table has a zero length name, so ar_name
    contains the string "/" padded with 15 blank characters on the right. A
    64-bit archive symbol table sets ar_name to the string "/SYM64/", padded
    with 9 blank characters to the right.
    
    [0]: https://docs.oracle.com/cd/E36784_01/html/E36873/ar.h-3head.html

--- test/test_gnu.py
+++ test/test_gnu.py
@@ -10,7 +10,15 @@ class GNUExtendedNames(unittest.TestCase):
 				list(ar.archived_files.keys()))
 		self.assertEqual(2, len(ar.headers))
 		ar.close()
-	
+
+	def test_single_name_64(self):
+		ar = arpy.Archive(os.path.join(os.path.dirname(__file__), 'gnu_single_name_64.ar'))
+		ar.read_all_headers()
+		self.assertEqual([b'a_very_long_name_for_the_gnu_type_header_so_it_can_overflow_the_standard_name_length'],
+				list(ar.archived_files.keys()))
+		self.assertEqual(2, len(ar.headers))
+		ar.close()
+
 	def test_multi_name_with_space(self):
 		ar = arpy.Archive(os.path.join(os.path.dirname(__file__), 'gnu_multi_names.ar'))
 		ar.read_all_headers()
